318fd24e0bcb679671e52ea1a8163df6cd1a6add
LuisCriollo-98 <lucho199802@gmail.com>
2026-05-05 21:11:33 -0500

    iniciando dashboard de admin

---
+++ frontend/components/ui/AdminNav.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Logo from "@/components/ui/Logo";
+import { logout } from "@/actions/auth-actions";
+
+export default function AdminNav() {
+  return (
+    <header className="px-10 py-5 bg-white flex justify-between">
+      <div className="flex gap-5 text-white">
+        <Logo />
+      </div>
+
+      <div className="flex gap-2 items-center">
+        <Link
+          href={"/admin/products"}
+          className="rounded text-black font-bold p-2"
+        >
+          Productos
+        </Link>
+
+        <Link
+          href={"/admin/orders"}
+          className="rounded text-black font-bold p-2"
+        >
+          Pedidos
+        </Link>
+        <Link
+          href={"/admin/categories"}
+          className="rounded text-black font-bold p-2"
+        >
+          Categorias
+        </Link>
+
+        <Link href={"/"} className="rounded bg-green-400 font-bold py-2 px-10">
+          Tienda
+        </Link>
+
+        <form action={logout}>
+          <button
+            type="submit"
+            className="rounded text-black cursor-pointer font-bold py-2 px-10"
+          >
+            Cerrar Sesión
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
