fac042240e6671e2038b39709dee332e4c0df987
LuisCriollo-98 <lucho199802@gmai.com>
2026-05-10 20:03:16 -0500

    Funcion de actualizar productos terminado

---
+++ frontend/actions/update-product-action.ts
@@ -0,0 +1,53 @@
+"use server"
+
+import { ErrorResponseSchema, Product, ProductFormSchema } from "@/src/schemas"
+import { cookies } from "next/headers"
+
+type ActionStateType = {
+    errors: string[]
+    success: string
+}
+export async function updateProduct(productId: Product['id'], prevState: ActionStateType, formData: FormData) {
+    const product = ProductFormSchema.safeParse({
+        name: formData.get('name'),
+        price: formData.get('price'),
+        priceFinal: formData.get('priceFinal'),
+        inventory: formData.get('inventory'),
+        categoryId: formData.get('categoryId'),
+        municipalityId: formData.get('municipalityId'),
+        presentationId: formData.get('presentationId'),
+    })
+    if (!product.success) {
+        return {
+            errors: product.error.issues.map((issue) => issue.message),
+            success: ''
+        }
+    }
+    //Comunicar con la API a travez de una peticion 
+    const cookieStore = await cookies()
+    const token = cookieStore.get("auth_token")?.value
+
+    const url = `${process.env.API_URL}/products/${productId}`
+    const req = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(product.data),
+    })
+    const json = await req.json()
+
+    //validacion de la respuesta del servidor 
+    if (!req.ok) {
+        const errors = ErrorResponseSchema.parse(json)
+        return {
+            errors: errors.message.map(issue => issue),
+            success: ''
+        }
+    }
+    return {
+        errors: [],
+        success: 'Producto actualizado correctamente'
+    }
+}
--- frontend/components/products/EditProductForm.tsx
+++ frontend/components/products/EditProductForm.tsx
@@ -1,14 +1,36 @@
 "use client";
 import { useActionState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import { updateProduct } from "@/actions/update-product-action";
+
+
+type Params = { id: string }
 
 export default function EditProductForm({ children, }: { children: React.ReactNode }) {
     const router = useRouter()
+    const { id } = useParams<{ id: string }>()
 
+    const updateProductWithId = updateProduct.bind(null, +id)
+    const [state, dispath] = useActionState(updateProductWithId, {
+        errors: [],
+        success: ''
+    })
+
+    useEffect(() => {
+        if (state.errors) {
+            state.errors.forEach(error => toast.error(error))
+        }
+        if (state.success) {
+            toast.success(state.success)
+            router.push('/admin/products')
+        }
+    }, [state])
 
     return (
-        <form className="space-y-5">
+        <form className="space-y-5"
+            action={dispath}
+        >
             {children}
             <input
                 type="submit"
